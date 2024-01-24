9879ffb8e4cdba70dc5807707eec448ba284c906
Ivan Roa <naviroa92@Ivans-MacBook-Pro.local>
2024-01-23 21:55:27 -0500

    our services

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,38 @@ document.addEventListener('DOMContentLoaded', function () {
     xhr.open('GET', 'assets/partials/navbar.html', true);
     xhr.send();
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Cargar el contenido del footer.html en la sección con id "footer"
+    var footerContainer = document.getElementById('cta-section');
+    var xhr = new XMLHttpRequest();
+    
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                footerContainer.innerHTML = xhr.responseText;
+            } else {
+                console.error('Error al cargar el navbar: ' + xhr.status);
+            }
+        }
+    };
+    xhr.open('GET', 'assets/partials/cta-section.html', true);
+    xhr.send();
+});
+
+// Opcional: Puedes agregar más imágenes ajustando la lista de imágenes y la lógica del script.
+const images = ['imagen1.jpg', 'imagen2.jpg', 'imagen3.jpg', 'imagen4.jpg', 'imagen5.jpg', 'imagen6.jpg', 'imagen7.jpg', 'imagen8.jpg'];
+let currentSlide = 0;
+
+function updateSlide() {
+  const container = document.querySelector('.carousel-container');
+  container.style.transform = `translateX(-${currentSlide * 10}%)`;
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % images.length;
+  updateSlide();
+}
+
+// Opcional: Ajusta el intervalo según tus necesidades.
+setInterval(nextSlide, 5000); // Cambia la imagen cada 5 segundos.
