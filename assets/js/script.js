bf4ffc2205be4f3644d94fdaf2af3a5782acc88d
Ivan Roa <naviroa92@Ivans-MacBook-Pro.local>
2024-01-30 20:39:58 -0500

    value section

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,14 +17,14 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Cargar el contenido del footer.html en la sección con id "footer"
-    var footerContainer = document.getElementById('navbar');
+    // Cargar el contenido del navbar.html en la sección con id "navbar"
+    var navbarContainer = document.getElementById('navbar');
     var xhr = new XMLHttpRequest();
     
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
-                footerContainer.innerHTML = xhr.responseText;
+                navbarContainer.innerHTML = xhr.responseText;
             } else {
                 console.error('Error al cargar el navbar: ' + xhr.status);
             }
@@ -35,16 +35,16 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Cargar el contenido del footer.html en la sección con id "footer"
-    var footerContainer = document.getElementById('cta-section');
+    // Cargar el contenido del cta-section.html en la sección con id "cta-section"
+    var ctaContainer = document.getElementById('cta-section');
     var xhr = new XMLHttpRequest();
     
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
-                footerContainer.innerHTML = xhr.responseText;
+                ctaContainer.innerHTML = xhr.responseText;
             } else {
-                console.error('Error al cargar el navbar: ' + xhr.status);
+                console.error('Error al cargar el cta: ' + xhr.status);
             }
         }
     };
@@ -52,19 +52,22 @@ document.addEventListener('DOMContentLoaded', function () {
     xhr.send();
 });
 
-// Opcional: Puedes agregar más imágenes ajustando la lista de imágenes y la lógica del script.
-const images = ['imagen1.jpg', 'imagen2.jpg', 'imagen3.jpg', 'imagen4.jpg', 'imagen5.jpg', 'imagen6.jpg', 'imagen7.jpg', 'imagen8.jpg'];
-let currentSlide = 0;
+document.addEventListener('DOMContentLoaded', function () {
+    // Cargar el contenido del cta-section.html en la sección con id "value"
+    var valueContainer = document.getElementById('value');
+    var xhr = new XMLHttpRequest();
+    
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                valueContainer.innerHTML = xhr.responseText;
+            } else {
+                console.error('Error al cargar el value: ' + xhr.status);
+            }
+        }
+    };
+    xhr.open('GET', 'assets/partials/value.html', true);
+    xhr.send();
+});
 
-function updateSlide() {
-  const container = document.querySelector('.carousel-container');
-  container.style.transform = `translateX(-${currentSlide * 10}%)`;
-}
 
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % images.length;
-  updateSlide();
-}
-
-// Opcional: Ajusta el intervalo según tus necesidades.
-setInterval(nextSlide, 5000); // Cambia la imagen cada 5 segundos.
